419b1868eeadddcb22b1184f5e77e91733f4bd21
saltPillars <30701703+saltPillars@users.noreply.github.com>
2023-05-23 14:08:46 -0500

    Working with part of a list

--- lists.py
+++ lists.py
@@ -1,46 +1,80 @@
-# List Exercises 3
+# Working with part of a list
 
-# 4-3 Counting to Twenty
-for value in range(1, 21):
-	print(value)
+# A slice will create a specific group of items in a list
+# To create a slice you must specify the index of the first and last elements
+# Python stops one item before the second index
+# If you want the first 3 elements you would list 0 and 3 which would return elements 0, 1, 2
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[0:3])
+# Output: ['charles', 'martina', 'michael']
 
-print("\n--------------------\n")
+# If you omit the first index it starts at the beginning of the list
+print(players[:4])
+# Output: ['charles', 'martina', 'michael', 'florence']
 
-# 4-4 One Million
-#for value in range(1, 1_000_001):
-	#print(value)
+# If you omit the second index it starts at the provided index and goes to the end of the list
+print(players[2:])
+# Output: ['michael', 'florence', 'eli']
 
-print("\n--------------------\n")
+# Negative indexes work
+print(players[1:-3])
+# Output: ['martina']
 
-# 4-5 Summing a million
-million = range(1, 1_000_001)
-print(min(million))
-print(max(million))
-print(sum(million))
+print(players[-2:])
+# Output: ['florence', 'eli']
 
-print("\n--------------------\n")
-# 4-6 Odd Numbers
-odd_numbers = range(1, 21, 2)
-for number in odd_numbers:
-	print(number)
+print(players[:-1])
+# Output: ['charles', 'martina', 'michael', 'florence']
 
-print("\n--------------------\n")
-# 4-7 Threes
-threes = range(3, 31, 3)
-for number in threes:
-	print(number)
+print("\n------------------\n")
 
-print("\n--------------------\n")
-# 4-8 Cubes
-cubes = []
-for value in range(1, 11):
-	cubes.append(value ** 3)
+# Looping Through a Slice
 
-for cube in cubes:
-	print(cube)
+# You can use a slice in a for loop
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print("Here are the first three players on my team:")
+for player in players[:3]:
+	print(player.title())
 
-print("\n--------------------\n")
-# 4-9 Cube Comprension
-cubes = [value ** 3 for value in range(1, 11)]
-for cube in cubes:
-	print(cube)
+print("\n------------------\n")
+
+# Copying a List
+
+# you can store the value of a slice as a copy of a list
+# if no idexes are specified the entire list will be copied
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+
+print("My favorite foods are:")
+print(my_foods)
+
+print("\nMy friend's favorite foods are:")
+print(friend_foods)
+
+print("\n------------------\n")
+
+# The two lists are truly two seperate objects
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
+
+print("My favorite foods are:")
+print(my_foods)
+
+print("\nMy friend's favorite foods are:")
+print(friend_foods)
+
+print("\n------------------\n")
+
+# Assigning a list to a variable does not create a copy of the list
+# Instead it just references the original list (think points to original object)
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods
+
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
+
+print("My favorite foods are:")
+print(my_foods)
+
+print("\nMy friend's favorite foods are:")
+print(friend_foods)
