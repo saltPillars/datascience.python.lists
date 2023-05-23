e53f1fea973f239396b1f092843426e3d49af9b0
saltPillars <30701703+saltPillars@users.noreply.github.com>
2023-05-23 13:21:02 -0500

    Numerical Lists

--- lists.py
+++ lists.py
@@ -1,43 +1,68 @@
-# Indentation and Loop Errors 
+# Numerical Lists
 
-# Indentation determines how a line, or group of lines, is related
-# More complex code can have multiple levels of indentation
+# Lists are ideal for storing sets of numbers
 
-# Forgetting to Indent
-# Python will remind you if you forget to indent
-#magicians = ['alice', 'david', 'carolina']
-#for magician in magicians:
-#print(magician)
+# range() is used to generate a series of numbers
+# It starts the count at the number you provide
+# Don't fall into off-by-one errors
+for value in range(1, 5):
+	print(value)
+# Output: 1-4
 
-# Output: IndentationError: expected an indented block after 'for' statement on line 9
+print("\n---------\n")
 
-# Forgetting to Indent Additional Lines
-# If a loop is behaving weirdly make sure all lines in the loop are indented.
-# A runtime error wont be thrown here so you'll have to check you output an make sure its right
-magicians = ['alice', 'david', 'carolina']
-for magician in magicians:
-	print(f"{magician.title()}, that was a great trick!")
-print(f"I can't wait to see your next trick, {magician.title()}.\n")
+for value in range(1, 6):
+	print(value)
+# Output: 1-5
 
-# If you indent when it doesn't need to be Python will throw an error
-message = "Hello Python world!"
-#	print(message)
+print("\n---------\n")
 
-# Output: IndentationError: unexpected indent
+# You can also pass range() only one value and it will start at 0
+for value in range(10):
+	print(value)
+# Output: 1-9
 
-# If you indent a line after the loop that shouldn't it will be ran with the loop
-magicians = ['alice', 'david', 'carolina']
-for magician in magicians:
-	print(f"{magician.title()}, that was a great trick!")
-	print(f"I can't wait to see your next trick, {magician.title()}.\n")
-	
-	print("Thank you everyone, that was a great magic show!")
+print("\n---------\n")
 
-# Output: Prints all 3 lines for each item in the list
+# You can convert range into a list by wrapping range() with list()
+numbers = list(range(1,6))
+print(numbers)
+# Output: [1, 2, 3, 4, 5]
 
-# Forgetting the colon results in a syntax error
-magicians = ['alice', 'david', 'carolina']
-#for magician in magicians
-#	print(magician)
+# If you pass a third argument to range() Python uses it to size steps
+even_numbers = list(range(2, 11, 2))
+print(even_numbers)
+# Output: [2, 4, 6, 8, 10]
 
-# Output: SyntaxError: expected ':'
+# range() is very versatile 
+# To put the first 10 squares of numbers you could do:
+squares = []
+for value in range(1, 11):
+	square = value ** 2
+	squares.append(square)
+
+print(squares)
+# Output: [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# Can be written more concisely
+squares = []
+for value in range(1, 11):
+	squares.append(value ** 2)
+
+print(squares)
+
+# Python has built in functions for working with lists of numbers
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(min(digits))
+# Output: 1
+
+print(max(digits))
+# Output: 10
+
+print(sum(digits))
+# Output: 55
+
+# List Comprehensions allow you to generate complex lists (such as squares) in one line of code
+# List comprehension combines for loop and creation of new elements into one line and automatically appends each new element
+squares = [value ** 2 for value in range(1, 11)]
+print(squares)
