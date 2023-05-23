dccf5e573468c408c98b3cd5ab32840ee3a48e66
saltPillars <30701703+saltPillars@users.noreply.github.com>
2023-05-23 12:16:28 -0500

    Looping Through Lists

--- lists.py
+++ lists.py
@@ -1,16 +1,25 @@
-# Avoiding Index Errors
+# Looping Through Lists
 
-# Index errors are common. Occur when trying to access an element that doesn't exist
-motorcycles = ['honda', 'yamaha', 'suzuki']
+magicians = ['alice', 'david', 'carolina']
 
-# results in index error due to no element at index 3
-print(motorcycles[3])
+# for loop iterates through the entire loop
+# uses a temporary variable to be associated with item in list
+# make temporary variable meaningful name
+for magician in magicians:
+	print(magician)
 
-# Use bult in index indicators to avoid
-print(motorcycles[-1])
+# You can do anything you want in a for loop
+for magician in magicians:
+	print(f"{magician.title()}, that was a great trick!")
 
-# The only time -1 will result in index error is if list is empty
-motorcycles = []
-print(motorcycles[-1])
+# You can write as many lines in a loop as you want
+# Any line thta is indented is considered inside the loop
+for magician in magicians:
+	print(f"{magician.title()}, that was a great trick!")
+	print(f"I can't wait to see your next trick, {magician.title()}\n")
 
-# If index errors occur and you cant figure it out print the list and trace data
+#Any lines of code after the loop are executed once the loop ends
+for magician in magicians:
+	print(f"{magician.title()}, that was a great trick!")
+	print(f"I can't wait to see your next trick, {magician.title()}\n")
+print("Thank you, everyone. That was a great magic show!")
